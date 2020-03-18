017870e2a6b74330646206ab3ef27e01dcba73eb
Roman <47788675+Roman-Yarinski@users.noreply.github.com>
2020-03-18 23:19:14 +0300

    unit: task tepical-arrays-problems

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.max = function max (array) {
+  if (!(array && array.length >0)) return 0;
+  return array.sort((a, b) => a - b).pop();
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.min = function min (array) {
+  if (!(array && array.length >0)) return 0;
+ return array.sort((a, b) => b - a).pop();
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (!(array && array.length >0)) return 0;
+  return array.reduce((sum, current) => sum + current) / array.length;
 }
